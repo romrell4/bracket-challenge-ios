1392bd43b481b58fb9f05a6dcfe7f36179ec92d6
Eric Romrell <eric_romrell@byu.edu>
2020-02-02 20:20:44 -0700

    Added firebase initialization

--- BracketChallenge/support/AppDelegate.swift
+++ BracketChallenge/support/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FacebookCore
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+		FirebaseApp.initialize()
         return true
     }
 	
